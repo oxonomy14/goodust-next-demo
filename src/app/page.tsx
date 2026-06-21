63f9b2afad6a3dc1aa919606eeb78b1a04846c16
oxonomy14 <oxonomy14@gmail.com>
2026-06-22 00:08:34 +0700

    added section Services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import AboutUs from '@/components/AboutUs/AboutUs';
 import ContactForm from '@/components/ContactForm/ContactForm';
 import Testimonials from '@/components/Testimonials/Testimonials';
 import PromoBanner from '@/components/PromoBanner/PromoBanner';
+import Services from '@/components/Services/Services';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function Home() {
       <Hero />
       <AboutUs />
       <PromoBanner />
+      <Services />
       <ContactForm />
       <Testimonials />
     </LayoutShell>
